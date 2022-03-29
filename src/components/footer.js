1abc53dff4ae52e3be52f6a3baf57964bdf43ef0
Shreshth Verma <shreshth@shreshthverma.me>
2022-03-29 17:55:25 +0530

    feat: added covid safety guidelines and footer

---
+++ src/components/footer.js
@@ -0,0 +1,5 @@
+let innerHTML = `<h3>&copy; Voyages progressifs 2022</h3><div class="footer-links"><img src="public/img/facebook.svg" alt="Facebook"/><img src="public/img/twitter.svg" alt="Twitter"/><img src="public/img/instagram.svg" alt="Instagram"/></div>`
+
+let footer = document.querySelector('.footer');
+
+footer.innerHTML = innerHTML;
