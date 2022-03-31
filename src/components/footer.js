09d0821893e9aa876d3bd63255e1bce09b517602
Shreshth Verma <shreshth@shreshthverma.me>
2022-03-31 13:58:39 +0530

    Feat: completed the site... once and for all!

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-let innerHTML = `<h3>&copy; Voyages progressifs 2022</h3><div class="footer-links"><img class="footer-links-logo" src="public/img/facebook.svg" alt="Facebook"/><img class="footer-links-logo" src="public/img/twitter.svg" alt="Twitter"/><img class="footer-links-logo" src="public/img/instagram.svg" alt="Instagram"/></div>`
+let innerHTML = `<h3>&copy; Voyages progressifs 2022</h3><div class="footer-links"><img class="footer-links-logo" src="public/img/facebook.svg" alt="Facebook"/><img class="footer-links-logo" src="public/img/twitter.svg" alt="Twitter"/><img class="footer-links-logo" src="public/img/instagram.svg" alt="Instagram"/></div><p class="footer-text">This website was created by the students of DPS Kolar Rd. Bhopal - namely, Shreshth Verma, Dhairya Garg and Aditi Avdhani</p>`
 
 let footer = document.querySelector('.footer');
 
